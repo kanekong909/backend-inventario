cb50c3411033c7310ad2e8b6fe9cdf8c3d9a59f9
MateoBossa <mateobossa909@gmail.com>
2026-03-25 14:33:34 -0500

    Fix package.json JSON + prisma.config for Railway

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,8 +5,7 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
 
-  // Cambiamos env() por process.env para que no falle si la variable no existe en build
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL || "mysql://placeholder", // evita error si no existe en build
   },
 });
